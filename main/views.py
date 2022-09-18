79009a9e37ae75df8cabd6c2b1d6481f26cf45f0
Braylon Shu <hshu3770@uni.sydney.edu.au>
2022-09-18 11:56:16 +1000

    beauty index popular part

--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+from product.models import Product
+
 
 # Create your views here.
 def index(request):
-    return render(request, template_name="main/index.html", context={'hide_hr': True})
+    return render(request, template_name="main/index.html", context={'hide_hr': True, 'products': Product.objects.order_by("-id")})
 
 
 def fail(request, context):
