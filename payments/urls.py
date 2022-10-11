e237e9713a8e2b8cde65e0f78f512f1e7c49cf15
Braylon Shu <hshu3770@uni.sydney.edu.au>
2022-10-11 18:24:18 +1100

    Project Roughtly Completed!

--- payments/urls.py
+++ payments/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('success/', PaymentSuccessView.as_view(), name='success'),
     path('failed/', PaymentFailedView.as_view(), name='failed'),
     path('api/checkout-session/<id>/', create_checkout_session, name='api_checkout_session'),
+    path('cancel/<int:id>/', cancel_subscription, name='cancel_subscription'),
 ]
