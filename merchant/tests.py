669e9b3df70f2fdf6dd5b463966cdfc6b7c1b797
Yidong Gan <ygan7134@uni.sydney.edu.au>
2022-10-15 00:19:44 +0800

    fix upload test

--- merchant/tests.py
+++ merchant/tests.py
@@ -1,10 +1,13 @@
+import os
 from unittest import mock
 import json
 from django.conf import settings
 from django.core.files import File
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from product.forms import createProductForm, createAddressForm
 from product.models import Product, Address
 from user.models import User
 from merchant.models import Merchant
@@ -14,7 +17,8 @@ from merchant.models import Merchant
 class MerchantTestCase(TestCase):
     def setUp(self):
         self.user = User()
-        self.merchant = Merchant(user=self.user, abn_verified=True, abn='123', merchant_name='name', merchant_intro='info')
+        self.merchant = Merchant(user=self.user, abn_verified=True, abn='123', merchant_name='name',
+                                 merchant_intro='info')
         self.client = Client()
 
     def test_merhchant_model(self):
@@ -53,37 +57,70 @@ class MerchantTestCase(TestCase):
         response = self.client.post(login_url, data, follow=True)
         status_code = response.status_code
         redirect_path = response.request.get("PATH_INFO")
-        print("now we are"+redirect_path)
-        self.assertEqual(reverse("merchant_home"),redirect_path)
+        print("now we are" + redirect_path)
+        self.assertEqual(reverse("merchant_home"), redirect_path)
         self.assertEqual(status_code, 200)
         print("before")
         print(Product.objects.all().count())
         # p_form = ['name',100,100,'a brief one',mock.MagicMock(spec=File, name='FileMock')]
         # a_form = ['address line one','redforn','NSW',2020]
-        data = {
-            "product_type": 'accessories',
-            'product_name': 'name',
-            'price': 100.0,
-            'stock': 100,
-            'status': 'AVAILABLE',
-            'view_count': 1,
-            'is_rent': False,
-            'description': 'a brief one',
-            'image':'media/product_pics/fireball.png',
+        # data = {
+        #     "product_type": 'accessories',
+        #     'product_name': 'name',
+        #     'price': 100.0,
+        #     'stock': 100,
+        #     'status': 'AVAILABLE',
+        #     'view_count': 1,
+        #     'is_rent': False,
+        #     'description': 'a brief one',
+        #     'image': 'media/product_pics/fireball.png',
+        #     'address_line_1': '1',
+        #     'suburb': 'redforn',
+        #     'state': 'NSW',
+        #     'postcode': 2020
+        # }
+        #
+        # a_form = createAddressForm({
+        #     'address_line_1': '1',
+        #     'suburb': 'redforn',
+        #     'state': 'NSW',
+        #     'postcode': 2020
+        # })
+        #
+        # address = a_form.save()
+        #
+        # file_mock = mock.MagicMock(spec=File, name='FileMock')
+        # self.image = file_mock
+        # upload_file = open('media/media/product_pics/fireball.png', 'rb')
+        # form = createProductForm(
+        #     data={
+        #         'product_name': 'name',
+        #         'price': 100.0,
+        #         'stock': 100,
+        #         'description': 'a brief one',
+        #     },
+        #     files={'image': SimpleUploadedFile(upload_file.name, upload_file.read())}
+        # )
 
-            'address_line_1': '1',
-            'suburb': 'redforn',
-            'state': 'NSW',
-            'postcode': 2020
-
-        }
-        form = ProductForm()
-        form
-        response = self.client.post('/product/add_new_product',json.dumps(data),content_type="application/json")
-
-        print(response.status_code)
-        print("after")
-        print(Product.objects.all().count())
-
-
+        # self.client.post()
+        # print(form.is_valid())
+        # print(form.errors)
+        with open('media/media/product_pics/fireball.png', 'rb') as upload_file:
+            response = self.client.post(
+                "/product/add_new_product",
+                data={
+                    'product_name': 'name',
+                    'price': 100.0,
+                    'stock': 100,
+                    'description': 'a brief one',
+                    'image': upload_file,
+                    'address_line_1': '1',
+                    'suburb': 'redforn',
+                    'state': 'NSW',
+                    'postcode': 2020
+                }
+            )
+            print(response.status_code)
+            print("after")
+            print(Product.objects.all().count())
 
